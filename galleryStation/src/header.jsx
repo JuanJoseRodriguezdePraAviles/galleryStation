146cfae8f2c81efa20108ac368ed5bf8450f18fd
JuanJoseRodriguezdePraAviles <45094901+JuanJoseRodriguezdePraAviles@users.noreply.github.com>
2025-02-10 16:16:01 +0100

    basic header implemented

--- galleryStation/src/header.jsx
+++ galleryStation/src/header.jsx
@@ -1,7 +1,16 @@
+import './css/style.css';
+
 function Header() {
     return (
-        <header></header>
+        <header>
+            <div className='title'>
+                <p>Gallery Station</p>
+            </div>
+            <div className='avatar'>
+            </div>
+        </header>
     );
 
 }
 
+export default Header;
